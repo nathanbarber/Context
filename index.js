b157f463c6c4bd0fff2c3f20f0b35e76c0744dc2
Nathan Barber <nathanbarber@O2.local>
2017-09-07 13:35:48 -0400

    Front page redesign (Rev 1) and javascript redesign. Connection / disconnection / room joining

--- index.js
+++ index.js
@@ -1,111 +1,31 @@
-var express = require('express');
-var bodyparser = require('body-parser');
-var favicon = require('serve-favicon');
-var path = require('path');
+var express = require("express"),
+    http = require("http"),
+    socketio = require("socket.io"),
+    path = require("path"),
+    fav = require("serve-favicon"),
+    clientsConnected = 0,
+    port = 8084;
 
 var app = express();
-app.use(express.static(__dirname + '/public'));
-app.use('/socket', express.static(__dirname + '/node_modules/socket.io-client/dist/'));
-app.use(favicon(path.join(__dirname + "/public" + "/img" + "/favicon.png")));
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: false}));
+app.use("/socket", express.static(__dirname + '/node_modules/socket.io-client/dist/'));
+app.use(express.static(__dirname + "/public"));
+app.use(fav(path.join(__dirname + "/public/img/favicon.png")));
 
-var server = require("http").createServer(app);
-var io = require('socket.io')(server);
-server.listen(8084, function() {
-    console.log("listening on port 8083");
+var server = http.createServer(app);
+var io = socketio(server);
+server.listen(port, function() {
+    console.log("\t CONTEXT running on port " + port);
 });
 
-var connectionCount = 0;
-var softDisconnect = false;
-
-io.on('connection', function(socket) {
-    connectionCount++;
-    console.log("Client connected: " + connectionCount + " users online | " + new Date);
-    io.emit("count", {count: connectionCount, decrement: false});
-    socket.on("disc", function(data) {
-        connectionCount--;
-        console.log("Client disconnected: " + connectionCount + " users online | " + new Date);
-        io.emit("count", {count: connectionCount, decrement: false});
-        softDisconnect = true;
-        socket.disconnect();
-    })
-    socket.on("disconnect", function() {
-        if(softDisconnect == false) {
-            connectionCount--;``
-            console.log("Client disconnected: " + connectionCount + " users online | " + new Date);
-            io.emit("count", {count: connectionCount, decrement: true});
-            socket.disconnect();
-        } else {
-            softDisconnect = false;
-        }
-    })
-    socket.on('classify', function(data) {
-        var s = '/' + data.choice;
-        socket.join(s);
+io.on("connection", function(socket) {
+    clientsConnected++;
+    console.log(clientsConnected + " users online");
+    socket.on("choose-room", function(data) {
+        socket.join(data);
+        console.log(socket.id + " chose room " + data);
     });
-    socket.on('chat message', function(message) {
-        socket.broadcast.in(getRoom(socket)).emit("message", {text: message});
-        //console.log(getRoom(socket));
+    socket.on("disconnect", function(socket) {
+        clientsConnected--;
+        console.log(clientsConnected + " users online");
     });
-});
-
-function getRoom(client) {
-    var rooms = client.rooms;
-    for(var key in rooms) {
-        if(key.substring(0, 1) == '/') {
-            return key;
-        }
-    }
-}
-
-function userCount(room) {
-    var clients = io.nsps['/'].adapter.rooms[room];
-    if(clients == null || clients == undefined) {
-        return undefined;
-    } else {
-        return Object.keys(clients).length;
-    }
-}
-
-setInterval(function() {
-    var openrooms = io.sockets.adapter.rooms
-    var existing = new Array();
-    for(var i in openrooms) {
-        if(i.substring(0,1) == '/') {
-            existing.push(i);
-        }
-    }
-    for(var i = 0; i < existing.length; i++) {
-        var c = io.sockets.adapter.rooms[existing[i]].length;
-        //console.log(existing[i] + " with users: " + c);
-        io.in(existing[i]).emit("roomcount", {count: c});
-    }
-}, 300);
-    
-app.get('/activerooms', function(req, res) {
-    var finalreturn = new Array();
-    var openrooms = io.sockets.adapter.rooms
-    var existing = new Array();
-    for(var i in openrooms) {
-        if(i.substring(0,1) == '/') {
-            existing.push(i);
-        }
-    }
-    for(var i = 0; i < existing.length; i++) {
-        var c = io.sockets.adapter.rooms[existing[i]].length;
-        finalreturn.push({room: existing[i], online: c});
-    }
-    var ii = 0;
-    while(ii < finalreturn.length) {
-        if(ii > 0 && finalreturn[ii].online > finalreturn[ii-1].online) {
-            var temp = finalreturn[ii];
-            finalreturn[ii] = finalreturn[ii-1];
-            finalreturn[ii - 1] = temp;
-            ii--;
-        } else {
-            ii++;
-        }
-    }
-    res.send(finalreturn);
 });
--- public/app.js
+++ public/app.js
@@ -11,10 +11,70 @@ app.config(function($routeProvider) {
     });
 });
 
+var room = '';
+var name = '';
+var socket;
+
 app.controller('home', function($scope, $location) {
-    
+    if(socket) {
+        socket.disconnect();
+    }
+    $scope.hover = function() {
+        if($(".label").css("top") == "0px") {
+            TweenMax.to(".slider", 0.3, {top: "0%"});
+            TweenMax.to(".label", 0.3, {color: "red"});
+        }
+    };
+    $scope.leave = function() {
+        if($(".label").css("top") == "0px") {
+            TweenMax.to(".slider", 0.3, {top: "100%"});
+            TweenMax.to(".label", 0.3, {color: "white"});
+        }
+    };
+    $scope.initial = function() {
+        console.log("initial");
+        TweenMax.to(".label", 0.3, {top: "-100%"});
+        TweenMax.to(".slider", 0.3, {top: "-100%", onComplete: function() {
+            TweenMax.set(".slider", {top: "100%"});
+            TweenMax.to(".slider", 0.3, {top: "-100%"});
+            TweenMax.to("#room", 0.3, {top: "0%"});
+        }});
+
+    };
+    $scope.fromRoom = function(event) {
+        if(event.keyCode == 13) {
+            if($("#room").val() != '') {
+                $scope.room = $("#room").val().toUpperCase();
+                room = $scope.room;
+                TweenMax.to("#room", 0.3, {top: "-100%"});
+                TweenMax.to(".slider", 0.3, {top: "-100%", onComplete: function() {
+                    TweenMax.set(".slider", {top: "100%"});
+                    TweenMax.to("#name", 0.3, {top: 0, onComplete: function() {
+                        $("#name").focus();
+                    }});
+                }});
+            }
+        }
+    };
+    $scope.fromName = function() {
+        if(event.keyCode == 13) {
+            if($("#name").val() != '') {
+                $scope.name = $("#name").val();
+                name = $scope.name;
+                $location.path("/com");
+            }
+        }
+    };
+    $scope.revertButton = function() {
+        TweenMax.to("#button input", 0.3, {top: "100%", onComplete: function() {
+            TweenMax.to(".slider", 0.3, {top: "100%"});
+            TweenMax.to(".label", 0.3, {top: 0, color: "white"});
+            $("#button input").val('');
+        }});
+    };
 });
 
 app.controller('com', function($scope, $location) {
-
+    socket = io.connect();
+    socket.emit("choose-room", room);
 });
